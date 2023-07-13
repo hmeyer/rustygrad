560f168c60fa476781690f640f82af7b6348c2ab
Henning Meyer <tutmann@gmail.com>
2023-07-13 14:37:26 +0200

    simplified trainer

--- examples/point_classifier.rs
+++ examples/point_classifier.rs
@@ -2,13 +2,13 @@ use rustygrad::{v, Mlp, Module};
 
 
 fn main() {
-    let mut mlp = Mlp::new(2, &[8, 8, 1]);
+    let mut mlp = Mlp::new(2, &[6, 6, 1]);
     println!("{} - {} parameters", mlp, mlp.parameters().len());
     let xs = [[1.0, 1.0], [1.0, -1.0], [-1.0, 1.0], [-1.0, -1.0]];
     let ys = [-1.0, 1.0, 1.0, -1.0];
 
-    let step_size = 0.1;
-    for step in 0..100 {
+    let step_size = 0.25;
+    for step in 0..15 {
         // Always zero_grad.
         mlp.zero_grad();
         // Reset loss to zero.
@@ -28,8 +28,6 @@ fn main() {
             p.set_data(p.data() + p.grad() * -step_size);
         }
         // Log progress.
-        if step % 10 == 0 {
-            println!("step {} - loss = {:0.4} step_size = {}", step, loss.data(), step_size);
-        }
+        println!("step {} - loss = {:0.4} step_size = {:0.2}", step, loss.data(), step_size);
     }
 }
